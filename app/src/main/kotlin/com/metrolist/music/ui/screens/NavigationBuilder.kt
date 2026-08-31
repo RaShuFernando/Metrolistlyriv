c99473f57a8bc06f931580374798f593e2503411
RaShuFernando <rashmiupeksha07281@gmail.com>
2026-08-31 22:38:00 +0530

    feat: implement central navigation builder for app screens

--- app/src/main/kotlin/com/metrolist/music/ui/screens/NavigationBuilder.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/NavigationBuilder.kt
@@ -49,6 +49,7 @@ import com.metrolist.music.ui.screens.settings.AppearanceSettings
 import com.metrolist.music.ui.screens.settings.BackupAndRestore
 import com.metrolist.music.ui.screens.settings.ContentSettings
 import com.metrolist.music.ui.screens.settings.DarkMode
+import com.metrolist.music.ui.screens.settings.OverlayLyricsSettingsScreen
 import com.metrolist.music.ui.screens.settings.PlayerSettings
 import com.metrolist.music.ui.screens.settings.PrivacySettings
 import com.metrolist.music.ui.screens.settings.RomanizationSettings
@@ -373,12 +374,9 @@ fun NavGraphBuilder.navigationBuilder(
     }
 
     composable("settings/overlay_lyrics") {
-        com.metrolist.music.ui.screens.settings.OverlayLyricsSettingsScreen(navController)
+        OverlayLyricsSettingsScreen(navController)
     }
 
-    composable("settings/stream_sources") {
-        StreamSourcesSettings(navController)
-    }
     composable("settings/storage") {
         StorageSettings(navController)
     }
